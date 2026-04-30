9343eb3750151f169e293550680d8b91c30c21dd
Kaloyan Stefanov <kokmarok@gmail.com>
2026-04-30 16:46:49 +0300

    update driver with changes from esp32 version

--- Include/shared_values.h
+++ Include/shared_values.h
@@ -8,28 +8,53 @@
 #include <stdint.h>
 #include "FreeRTOS.h"
 #include "semphr.h"
+#include "event_groups.h"
 
 typedef enum {
     SHVAL_ERROR_OK,
     SHVAL_ERROR_VAL_UNAVAILABLE,
-    SHVAL_ERROR_SUB_QUEUE_FULL
+    SHVAL_ERROR_SUB_QUEUE_FULL,
+    SHVAL_ERROR_TIMEOUT
 } SHVAL_ErrorTypeDef;
 
 typedef struct {
     uint32_t InitialValue;
-    uint32_t SubscribersQueueSize;
+    EventBits_t SubscribersEventBits;
 } SHVAL_ConfigTypeDef;
 
 typedef struct {
     uint32_t Value;
     SemaphoreHandle_t Mutex;
 
-    QueueHandle_t SubscribersQueue;
-    uint32_t SubscribersCount;
+    EventGroupHandle_t EventGroup;
+    EventBits_t SubscribersEventBits;
 } SHVAL_HandleTypeDef;
 
+typedef struct {
+    void *InitialValue;
+    uint32_t ValueLen;
+    EventBits_t SubscribersEventBits;
+} SHVAL_PointerConfigTypeDef;
+
+typedef struct {
+    void *Value;
+    uint32_t ValueLen;
+    SemaphoreHandle_t Mutex;
+
+    EventGroupHandle_t EventGroup;
+    EventBits_t SubscribersEventBits;
+} SHVAL_PointerHandleTypeDef;
+
 SHVAL_HandleTypeDef SHVAL_Init(SHVAL_ConfigTypeDef *Config);
+uint8_t SHVAL_Exists(SHVAL_HandleTypeDef *hshval);
 SHVAL_ErrorTypeDef SHVAL_GetValue(const SHVAL_HandleTypeDef *hshval, uint32_t *Value, uint32_t TimeoutMS);
 SHVAL_ErrorTypeDef SHVAL_SetValue(SHVAL_HandleTypeDef *hshval, uint32_t Value, uint32_t TimeoutMS);
+SHVAL_ErrorTypeDef SHVAL_WaitForValue(SHVAL_HandleTypeDef *hshval, EventBits_t BitToWait, uint32_t *Value, TickType_t TicksToWait);
+
+SHVAL_PointerHandleTypeDef SHVAL_PointerInit(SHVAL_PointerConfigTypeDef *Config);
+uint8_t SHVAL_PointerExists(SHVAL_PointerHandleTypeDef *hshval);
+SHVAL_ErrorTypeDef SHVAL_PointerGetValue(const SHVAL_PointerHandleTypeDef *hshval, void *Value, uint32_t *ValueLen, uint32_t TimeoutMS);
+SHVAL_ErrorTypeDef SHVAL_PointerSetValue(SHVAL_PointerHandleTypeDef *hshval, void *Value, uint32_t TimeoutMS);
+SHVAL_ErrorTypeDef SHVAL_PointerWaitForValue(SHVAL_PointerHandleTypeDef *hshval, EventBits_t BitToWait, void *Value, uint32_t *ValueLen, TickType_t TicksToWait);
 
 #endif //SHARED_VALUES_H
--- Src/shared_values.c
+++ Src/shared_values.c
@@ -4,20 +4,37 @@
 
 #include "shared_values.h"
 
+#include <string.h>
+
+/**
+ * @brief Initializes shared value using the provided config structure
+ * @param Config Config structure
+ */
 SHVAL_HandleTypeDef SHVAL_Init(SHVAL_ConfigTypeDef *Config) {
     SHVAL_HandleTypeDef handle = {
         .Value = Config->InitialValue,
+        .EventGroup = xEventGroupCreate(),
+        .SubscribersEventBits = Config->SubscribersEventBits,
         .Mutex = xSemaphoreCreateMutex()
     };
-
-    if (Config->SubscribersQueueSize > 0) {
-        handle.SubscribersQueue = xQueueCreate(Config->SubscribersQueueSize, sizeof(uint32_t));
-        handle.SubscribersCount = Config->SubscribersQueueSize;
-    }
 
     return handle;
 }
 
+/**
+ * @brief Checks whether the shared value is initialized
+ * @param hshval Shared Value Handle
+ */
+uint8_t SHVAL_Exists(SHVAL_HandleTypeDef *hshval) {
+    return hshval->Mutex != NULL;
+}
+
+/**
+ * @brief Gets shared value WITHOUT copying it
+ * @param hshval Shared Value Handle
+ * @param Value Pointer, which will be assigned to the value's location
+ * @param TimeoutMS Timeout in milliseconds
+ */
 SHVAL_ErrorTypeDef SHVAL_GetValue(const SHVAL_HandleTypeDef *hshval, uint32_t *Value, uint32_t TimeoutMS) {
     if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
         *Value = hshval->Value;
@@ -28,26 +45,135 @@ SHVAL_ErrorTypeDef SHVAL_GetValue(const SHVAL_HandleTypeDef *hshval, uint32_t *V
     }
 }
 
+/**
+ * @brief Sets shared value
+ * @param hshval Shared Value Handle
+ * @param Value Value to set
+ * @param TimeoutMS Timeout in milliseconds
+ */
 SHVAL_ErrorTypeDef SHVAL_SetValue(SHVAL_HandleTypeDef *hshval, uint32_t Value, uint32_t TimeoutMS) {
     if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
-        if (hshval->Value == Value) {
-            xSemaphoreGive(hshval->Mutex);
-            return SHVAL_ERROR_OK;
-        }
+        xEventGroupClearBits(hshval->EventGroup, hshval->SubscribersEventBits);
 
         hshval->Value = Value;
 
-        if (hshval->SubscribersQueue) {
-            // Queue is reset. All previous values are invalid
-            xQueueReset(hshval->SubscribersQueue);
-            for (int i = 0; i < hshval->SubscribersCount; i++) {
-                xQueueSend(hshval->SubscribersQueue, &Value, pdMS_TO_TICKS(TimeoutMS));
-            }
-        }
+        xEventGroupSetBits(hshval->EventGroup, hshval->SubscribersEventBits);
 
         xSemaphoreGive(hshval->Mutex);
         return SHVAL_ERROR_OK;
     } else {
         return SHVAL_ERROR_VAL_UNAVAILABLE;
     }
+}
+
+/**
+ * @brief Waits for shared value for 'TicksToWait' period and gets it WITHOUT copying it
+ * @param hshval Shared Value Handle
+ * @param BitToWait Event bit to wait for
+ * @param Value Pointer, which will be assigned to the value's location
+ * @param TicksToWait FreeRTOS ticks to wait
+ */
+SHVAL_ErrorTypeDef SHVAL_WaitForValue(SHVAL_HandleTypeDef *hshval, EventBits_t BitToWait, uint32_t *Value, TickType_t TicksToWait) {
+    SHVAL_ErrorTypeDef shval_err = SHVAL_ERROR_OK;
+    EventBits_t bits = xEventGroupWaitBits(hshval->EventGroup, BitToWait, pdTRUE, pdFALSE, TicksToWait);
+
+    if ((bits & BitToWait) != 0) {
+        if ((shval_err = SHVAL_GetValue(hshval, Value, pdTICKS_TO_MS(TicksToWait))) != SHVAL_ERROR_OK) {
+            return shval_err;
+        }
+        return SHVAL_ERROR_OK;
+    }
+
+    return SHVAL_ERROR_TIMEOUT;
+}
+
+/**
+ * @brief Initializes shared value using the provided config structure
+ * @note The `*Pointer*` methods are used for non uint32_t values
+ * @param Config Config structure
+ */
+SHVAL_PointerHandleTypeDef SHVAL_PointerInit(SHVAL_PointerConfigTypeDef *Config) {
+    SHVAL_PointerHandleTypeDef handle = {
+        .Value = Config->InitialValue,
+        .ValueLen = Config->ValueLen,
+        .EventGroup = xEventGroupCreate(),
+        .SubscribersEventBits = Config->SubscribersEventBits,
+        .Mutex = xSemaphoreCreateMutex()
+    };
+
+    return handle;
+}
+
+/**
+ * @brief Checks whether the shared value is initialized
+ * @note The `*Pointer*` methods are used for non uint32_t values
+ * @param hshval
+ */
+uint8_t SHVAL_PointerExists(SHVAL_PointerHandleTypeDef *hshval) {
+    return hshval->Mutex != NULL;
+}
+
+/**
+ *
+ * @param hshval Shared Value Handle
+ * @param Value Pointer, which will be assigned to the value's location
+ * @param ValueLen Length of data
+ * @param TimeoutMS Timeout in milliseconds
+ * @note The `*Pointer*` methods are used for non uint32_t values
+ * @return
+ */
+SHVAL_ErrorTypeDef SHVAL_PointerGetValue(const SHVAL_PointerHandleTypeDef *hshval, void *Value, uint32_t *ValueLen,
+                                         uint32_t TimeoutMS) {
+    if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
+        memcpy(Value, hshval->Value, hshval->ValueLen);
+        if (ValueLen != NULL) *ValueLen = hshval->ValueLen;
+        xSemaphoreGive(hshval->Mutex);
+        return SHVAL_ERROR_OK;
+    } else {
+        return SHVAL_ERROR_VAL_UNAVAILABLE;
+    }
+}
+
+/**
+ * @brief Sets shared value by COPYING the provided 'Value'
+ * @param hshval Shared Value Handle
+ * @param Value Value to set (length will be inferred from 'hshval')
+ * @param TimeoutMS Timeout in milliseconds
+ * @note The `*Pointer*` methods are used for non uint32_t values
+ */
+SHVAL_ErrorTypeDef SHVAL_PointerSetValue(SHVAL_PointerHandleTypeDef *hshval, void *Value,
+                                         uint32_t TimeoutMS) {
+    if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
+        memcpy(hshval->Value, Value, hshval->ValueLen);
+
+        xEventGroupSetBits(hshval->EventGroup, hshval->SubscribersEventBits);
+
+        xSemaphoreGive(hshval->Mutex);
+        return SHVAL_ERROR_OK;
+    } else {
+        return SHVAL_ERROR_VAL_UNAVAILABLE;
+    }
+}
+
+/**
+ * @brief Waits for shared value for 'TicksToWait' period and gets it WITHOUT copying it
+ * @param hshval Shared Value Handle
+ * @param BitToWait Event bit to wait for
+ * @param Value Pointer, which will be assigned to the value's location
+ * @param ValueLen (optional) Length of data
+ * @param TicksToWait FreeRTOS ticks to wait
+ * @note The `*Pointer*` methods are used for non uint32_t values
+ */
+SHVAL_ErrorTypeDef SHVAL_PointerWaitForValue(SHVAL_PointerHandleTypeDef *hshval, EventBits_t BitToWait, void *Value, uint32_t *ValueLen, TickType_t TicksToWait) {
+    SHVAL_ErrorTypeDef shval_err = SHVAL_ERROR_OK;
+    EventBits_t bits = xEventGroupWaitBits(hshval->EventGroup, BitToWait, pdTRUE, pdFALSE, TicksToWait);
+
+    if ((bits & BitToWait) != 0) {
+        if ((shval_err = SHVAL_PointerGetValue(hshval, Value, ValueLen, pdTICKS_TO_MS(TicksToWait))) != SHVAL_ERROR_OK) {
+            return shval_err;
+        }
+        return SHVAL_ERROR_OK;
+    }
+
+    return SHVAL_ERROR_TIMEOUT;
 }
